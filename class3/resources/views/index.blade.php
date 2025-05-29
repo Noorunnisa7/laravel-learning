c05b74d2e43670c0648b556c5da0bf76d0baadaa
noorunnisa7 <noorunnisa560@gmail.com>
2025-05-29 17:54:44 +0500

    Controllers

---
+++ class3/resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Welcome to my website</h1>
+    <section class="container">
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <h1 class="text-center">fill this form</h1>
+                <form method="post" action="{{Route('formSubmit')}}">
+                    @csrf
+                    <input type="text" placeholder="Enter Name" name="name" class="form-control mb-3">
+                    <input type="email" placeholder="Enter email" name="email" class="form-control mb-3">
+                    <input type="number" placeholder="Enter Number" name="number" class="form-control mb-3">
+                    <input type="text" placeholder="Enter address" name="address" class="form-control mb-3">
+                    <button class="btn btn-dark w-100">Submit</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ class3/resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    @include('header')
+    <h1>Welcome to laravel</h1>
+
+<?php
+echo "Laravel Learning";
+?>
+
+@php 
+$username = "Admin";
+$email = "Admin@gmail.com";
+@endphp
+
+
+<h2>
+    {{$username}}
+    {{$email}}
+</h2>
+
+@if($username == "Admin")
+<h3>Condition is true {{$username}}</h3>  
+@else 
+<h3>Condition is false {{$username}}</h3>
+@endif
+
+
+@php 
+$array = [1,2,3,4,5,6,7,8];
+@endphp
+
+<ul>
+@foreach($array as $value)
+<li>{{$value}}</li>
+@endforeach
+</ul>
+</body>
+</html>
